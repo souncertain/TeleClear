ed1ead87ea3a2c2adfbcb6a523d68a4bfd9505f1
souncertain <mafet170904@gmail.com>
2024-06-02 16:33:34 +0300

    TeleClear, ready to use.

---
+++ Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace TeleClear2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(45, 72);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(158, 26);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.Location = new System.Drawing.Point(40, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(396, 29);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Enter a phone number to send code";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.Location = new System.Drawing.Point(248, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 29);
+            this.label2.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.button1.Location = new System.Drawing.Point(45, 119);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(158, 39);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "SendCode";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.button2.Location = new System.Drawing.Point(404, 119);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(158, 39);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Confirm";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label3.Location = new System.Drawing.Point(40, 172);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(584, 29);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Enter the amount of unread messages into a channels";
+            this.label3.Visible = false;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.button3.Location = new System.Drawing.Point(45, 258);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(203, 39);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "LeaveChannels";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(45, 214);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(158, 26);
+            this.textBox3.TabIndex = 10;
+            this.textBox3.Visible = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(404, 72);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(158, 26);
+            this.textBox2.TabIndex = 11;
+            this.textBox2.Visible = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(316, 214);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(454, 204);
+            this.listBox1.TabIndex = 12;
+            this.listBox1.Visible = false;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.button4.Location = new System.Drawing.Point(45, 327);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(203, 39);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "Stop";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Visible = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "TeleClear";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WTelegram;
+using TL;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Diagnostics;
+using System.Threading;
+
+namespace TeleClear2
+{
+    public partial class Form1 : Form
+    {
+        private static string _info = "";
+        private static bool _stoped = false;
+        private WTelegram.Client _client;
+
+        public Form1()
+        {
+            InitializeComponent();
+            WTelegram.Helpers.Log = (l, s) => Debug.WriteLine(s);
+
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            _client?.Dispose();
+            Properties.Settings.Default.Save();
+        }
+
+        private async Task Login(string Info)
+        {
+            string what = await _client.Login(Info);
+            if (what != null)
+            {
+                label2.Text = what + ':';
+                textBox2.Text = "";
+                label2.Visible = textBox2.Visible = button2.Visible = true;
+                textBox2.Focus();
+                return;
+            }
+            textBox3.Visible = label3.Visible = button3.Visible = listBox1.Visible = button4.Visible = true;
+            listBox1.Items.Add($"Now we are connected as {_client.User}");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void button1_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                button1.Enabled = false;
+                _info = textBox1.Text;
+                _client = new WTelegram.Client(22034282, "50fe24f54e972eb9cad59b92cad750e8");
+                await Login(_info);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private async void button2_Click_1(object sender, EventArgs e)
+        {
+            label2.Visible = textBox2.Visible = button2.Visible = false;
+            await Login(textBox2.Text);
+        }
+
+        private async void button3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            if(_client.User == null)
+            {
+                MessageBox.Show("You must login!");
+                return;
+            }
+            var chats = await _client.Messages_GetAllChats();
+
+            long accessHash = 0;
+            int messageToLeave = Int32.Parse(textBox3.Text);
+            foreach (var chat in chats.chats.Values)
+            {
+                if (_stoped) break;
+                if(chat == null || !chat.IsActive) continue;
+                int unreadMessages = 0;
+                if (chat.IsChannel)
+                {
+                    try
+                    {
+                        TL.Channel channel = chats.chats.Values.ToList().First(x => x.ID == chat.ID) as TL.Channel;
+                        if (channel != null && channel.IsActive)
+                        {
+                            accessHash = channel.access_hash;
+                            Messages_ChatFull fullChannel = await _client.Channels_GetFullChannel(channel);
+                            if (fullChannel != null)
+                            {
+                                ChannelFull channelFull = fullChannel.full_chat as ChannelFull;
+                                if (channelFull != null)
+                                {
+                                    unreadMessages = channelFull.unread_count;
+                                    if (unreadMessages > messageToLeave)
+                                    {
+                                        await _client.Channels_LeaveChannel(new InputChannel(chat.ID, accessHash));
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        continue;
+                    }
+                }
+                else
+                {
+                    try
+                    {
+                        var FullChat = await _client.GetFullChat(chat);
+                        if (FullChat != null)
+                        {
+                            ChannelFull chatFull = FullChat.full_chat as ChannelFull;
+                            if (chatFull != null)
+                            {
+                                unreadMessages = chatFull.unread_count;
+                                if (unreadMessages > messageToLeave)
+                                {
+                                    TL.Channel channel = chat as TL.Channel;
+                                    if (channel != null && channel.IsActive)
+                                    {
+                                        try
+                                        {
+                                            await _client.Channels_LeaveChannel(new InputChannel(channel.ID, channel.access_hash));
+                                        }
+                                        catch (Exception)
+                                        {
+                                            continue;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        continue;
+                    }
+
+                }
+                listBox1.Items.Add($"Checked {chat.Title}, going to next");
+                Thread.Sleep(1000);
+            }
+            listBox1.Items.Clear();
+            listBox1.Items.Add($"Now we are connected as {_client.User}");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            _stoped = true;
+        }
+    }
+}
