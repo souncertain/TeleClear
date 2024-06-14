920e0cac2e567e0b23deda1a073941adeb8c7af4
souncertain <mafet170904@gmail.com>
2024-06-14 16:31:42 +0300

    Bug fix with stop button and added comments

--- Form1.cs
+++ Form1.cs
@@ -14,27 +14,32 @@ using System.Diagnostics;
 using System.Threading;
 using configApi;
 
+//Created by souncertain
+
 namespace TeleClear2
 {
     public partial class Form1 : Form
     {
-        private static string _info = "";
+        //variable stores info that the user enters
+        private static string _WhatWeNeedToLogin = "";
+        //variable stores condithion to stop leave channels
         private static bool _stoped = false;
+        //Telegram Client
         private WTelegram.Client _client;
 
         public Form1()
         {
             InitializeComponent();
-            WTelegram.Helpers.Log = (l, s) => Debug.WriteLine(s);
-
         }
 
+        //Save settings and frees up the client memory
         private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
         {
             _client?.Dispose();
             Properties.Settings.Default.Save();
         }
 
+        //Login Task, we take the information that we need to login and send this with TelegramAPI
         private async Task Login(string Info)
         {
             string what = await _client.Login(Info);
@@ -51,20 +56,22 @@ namespace TeleClear2
             listBox1.Items.Add($"Now we are connected as {_client.User}");
         }
 
+        //Method to change the text (what we need to enter right now) in login 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
         }
 
+        //Method to send code with a number of user
         private async void button1_Click_1(object sender, EventArgs e)
         {
             try
             {
                 
                 button1.Enabled = false;
-                _info = textBox1.Text;
+                _WhatWeNeedToLogin = textBox1.Text;
                 _client = new WTelegram.Client(config.appId, config.hash);
-                await Login(_info);
+                await Login(_WhatWeNeedToLogin);
             }
             catch (Exception ex)
             {
@@ -72,12 +79,16 @@ namespace TeleClear2
             }
         }
 
+        //Method to enter code from telegram and password
         private async void button2_Click_1(object sender, EventArgs e)
         {
             label2.Visible = textBox2.Visible = button2.Visible = false;
             await Login(textBox2.Text);
         }
 
+        //Main logic to clear channels, we take all user chats 
+        //and find where the amount of unread messages more than
+        //value which user enter
         private async void button3_Click(object sender, EventArgs e)
         {
             listBox1.Items.Clear();
@@ -86,6 +97,7 @@ namespace TeleClear2
                 MessageBox.Show("You must login!");
                 return;
             }
+            _stoped = false;
             var chats = await _client.Messages_GetAllChats();
 
             long accessHash = 0;
@@ -159,12 +171,14 @@ namespace TeleClear2
 
                 }
                 listBox1.Items.Add($"Checked {chat.Title}, going to next");
-                Thread.Sleep(1000);
+                Thread.Sleep(1000); //Need sleep because the TelegramApi
+                                    //has a limit of the number of requests per second
             }
             listBox1.Items.Clear();
             listBox1.Items.Add($"Now we are connected as {_client.User}");
         }
 
+        //Stop sending messages
         private void button4_Click(object sender, EventArgs e)
         {
             _stoped = true;
