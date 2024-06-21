9d7b9414aff749eb88ecfb318301e92e5ffe293e
souncertain <mafet170904@gmail.com>
2024-06-22 00:05:16 +0300

    mini update

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -251,6 +251,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "TeleClear";
             this.ResumeLayout(false);
