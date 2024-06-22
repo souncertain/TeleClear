630ea1756e6eca85f21400bb682ffb039419be2c
souncertain <mafet170904@gmail.com>
2024-06-22 21:25:19 +0300

    Start work for auth

--- config.cs
+++ config.cs
@@ -8,7 +8,8 @@ namespace configApi
 {
      class config
     {
-        public static int appId = 22034282;
-        public static string hash = "50fe24f54e972eb9cad59b92cad750e8";
+        public const int appId = 22034282;
+        public const string hash = "50fe24f54e972eb9cad59b92cad750e8";
+        public const string bot_token = "6992992553:AAFPcjgrETP7CrQQFkojI8hWmxTBj9AcTFo";
     }
 }
